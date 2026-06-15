4f66cd9019e0f093df892b1db7ffb9f71536c57a
Martyn Thomas <martyn.thomas1@hotmail.co.uk>
2026-06-15 20:31:27 +0100

    midi input support working

--- piano-hero/src/components/PianoGame.vue
+++ piano-hero/src/components/PianoGame.vue
@@ -1,18 +1,26 @@
 <script setup>
 import { ref } from "vue";
-import MidiDropLoader from "./piano-game/MidiLoader.vue";
+import MainMenu from "./piano-game/MainMenu.vue";
 import Canvas from "./piano-game/Canvas.vue";
 
+const view = ref("main-menu")
 const selectedFile = ref(null);
+
 
 function handleFileSelected(file) {
     selectedFile.value = file;
+    view.value = "game";
+}
+
+function navigateMainMenu() {
+    selectedFile.value = null;
+    view.value = 'main-menu';
 }
 </script>
 
 <template>
     <v-container fluid class="pa-0">
-        <MidiDropLoader v-if="!selectedFile" @file-selected="handleFileSelected" />
-        <Canvas v-if="selectedFile" v-model:file="selectedFile" />
+        <MainMenu v-if="view === 'main-menu'" @file-selected="handleFileSelected"/>
+        <Canvas v-if="view === 'game'" v-model:file="selectedFile" @navigate-main-menu="navigateMainMenu"/>
     </v-container>
 </template>
