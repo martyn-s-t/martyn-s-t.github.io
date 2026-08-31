3c3ba11eabe73cf678e66649b2f38a75617c3c2e
martyn-s-t <martyn.thomas1@hotmail.co.uk>
2026-08-31 23:56:01 +0000

    improve learn to play, add free-play gamemode

--- piano-hero/vite.config.js
+++ piano-hero/vite.config.js
@@ -3,6 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: './localhost-key.pem',
+      cert: './localhost.pem'
+    },
+    host: true,
+  },
   plugins: [vue()],
   base: '/piano-hero/dist/'
 })
