3193e291ce56bbbfb9b59baa4ce6e3c39b0e36e5
martyn-s-t <martyn.thomas1@hotmail.co.uk>
2026-09-02 22:33:00 +0000

    add title banner

--- piano-hero/src/components/piano-game/MainMenu.vue
+++ piano-hero/src/components/piano-game/MainMenu.vue
@@ -11,10 +11,20 @@ function startFreePlay() {
 
 <template>
     <v-container class="pa-4" style="max-width: 400px;">
+        <div class="title-wrapper">
+            <img src="https://placeholdit.com/1200x200/008080/FF69B4?text=PIANO+HERO" />
+        </div>
+
         <v-btn block class="mb-4" color="primary" @click="emit('navigate', 'song-manager')">Play a Song</v-btn>
         <v-btn block class="mb-4" color="primary" @click="startFreePlay">Free Play</v-btn>
         <v-btn block color="primary" @click="emit('navigate', 'settings')">Settings</v-btn>
     </v-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title-wrapper {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+</style>
